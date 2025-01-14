95b711d6b44236c7f80b3716cb77d31badec722c
mdismail07hossain <mdismail07hossain@gmail.com>
2025-01-14 13:00:41 +0600

    react

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+export default function App() {
+  return (
+   <>
+    <h1 className="text-7xl font-bold uppercase text-center my-5 py-5 bg-green-500 text-white">
+      Hello world!
+    </h1>
+    <div class="grid lg:grid-cols-3 gap-5 mx-5">
+    <div className="card bg-base-100  shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    <div className="card bg-base-100  shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    <div className="card bg-base-100  shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+</div>
+   </>
+  )
+}
